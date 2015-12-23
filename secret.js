da42d434bae5ccbe8764be01349e43516474c071
unknown <szhu@UTILE-T1700-22.Utile.local>
2015-12-23 11:34:33 -0500

    Change cartodb API key

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-	USER:'siqi',
-	API_KEY:'01a99487a4ea84bf854536db2bef429be762fdca',
+	USER:'massdevtdi',
+	API_KEY:'a2f9b785ef2c62299c7b55539b490dc8c177fd76'//'01a99487a4ea84bf854536db2bef429be762fdca',
 }
